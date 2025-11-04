4abc7f3d0188ddd5db576b92fd8a8f3e4d02d34f
Brendan Aeria <brendan.cloudstorage@gmail.com>
2025-11-03 21:43:32 -0800

    modified training to have sweep instead

--- TrainingDANN/main_train.py
+++ TrainingDANN/main_train.py
@@ -8,13 +8,11 @@ import gc
 # load data 
 model_name=str(sys.argv[1])
 mmap_neutral_path = str(sys.argv[2])
-mmap_HS_path = str(sys.argv[3])
-mmap_SS_path = str(sys.argv[4])
-mmap_target_path = str(sys.argv[5])
+mmap_sweep_path = str(sys.argv[3])
+mmap_target_path = str(sys.argv[4])
 
 mmap_neutral = np.load(mmap_neutral_path, mmap_mode='r') 
-mmap_HS = np.load(mmap_HS_path, mmap_mode='r') 
-mmap_SS = np.load(mmap_SS_path, mmap_mode='r')
+mmap_sweep = np.load(mmap_sweep_path, mmap_mode='r') 
 mmap_target = np.load(mmap_target_path, mmap_mode='r') 
 
 
@@ -31,5 +29,5 @@ model = create_model(mmap_neutral,loss_weights=initial_loss_weights)
 
 #train model
 print("TRAINING MODEL")
-model,score=train_model(model,mmap_neutral,mmap_HS,mmap_SS,mmap_target,val_split=val_split,batch_size=batch_size,loss_weights=initial_loss_weights,path=model_name) #GRL_HS_DuringBottleneck_trained
+model,score=train_model(model,mmap_neutral,mmap_sweep,mmap_target,val_split=val_split,batch_size=batch_size,loss_weights=initial_loss_weights,path=model_name) #GRL_HS_DuringBottleneck_trained
 
