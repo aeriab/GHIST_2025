b3952a11e08438ad2d97922e47588b44c5c27a04
Brendan Aeria <brendan.cloudstorage@gmail.com>
2025-11-03 21:12:50 -0800

    data is generated

--- ProcessingData/testingReshaping.py
+++ ProcessingData/testingReshaping.py
@@ -1,12 +1,12 @@
 import numpy as np
 import sys
 
-data = np.load('HS_sims.npy')
+data = np.load('N_sims.npy')
 
 new_data = data[..., 0]
 
 # 3. Save the new array to a new file
-np.save('sweeps.npy', new_data)
+np.save('neutrals.npy', new_data)
 
 # Optional: Print to confirm the shapes
 print(f"Original shape: {data.shape}")
